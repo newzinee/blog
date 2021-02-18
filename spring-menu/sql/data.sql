0ca7b04a73dbeb6d2cb79021ae90c2c4e744c3ba
treasureBear94 <qvo7896@gmail.com>
2021-02-18 23:41:52 +0900

    :memo: 메뉴 data 추가

---
+++ spring-menu/sql/data.sql
@@ -0,0 +1,6 @@
+insert into menu(id, parent_id, name, list_order) values(1, null, '1. 메뉴', 1);
+insert into menu(id, parent_id, name, list_order) values(2, null, '2. 메뉴', 2);
+insert into menu(id, parent_id, name, list_order) values(3, null, '3. 메뉴', 3);
+insert into menu(id, parent_id, name, list_order) values(4, 1, '1-1. 하위 메뉴', 1);
+insert into menu(id, parent_id, name, list_order) values(5, 1, '1-2. 하위 메뉴', 2);
+insert into menu(id, parent_id, name, list_order) values(6, 2, '2-1. 하위 메뉴', 1);
